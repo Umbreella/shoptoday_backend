8518097c1f5bf0b5ad2aa68899ce665ed906bb57
Umbreella <draginvadim@gmail.com>
2023-06-26 13:37:35 +0600

    Add Github Actions

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-import asyncio
-import platform
 import sys
-from asyncio import WindowsSelectorEventLoopPolicy
 
 import pytest
 
 if __name__ == '__main__':
-    if platform.system() == 'Windows':
-        asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-
     if len(sys.argv) > 1:
         pytest.main(args=['--disable-warnings', '-vv', *sys.argv])
     else:
