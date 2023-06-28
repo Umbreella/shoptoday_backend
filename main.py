e7a992b635bd564dcff2b9325d45ee84adec9721
Umbreella <draginvadim@gmail.com>
2023-06-28 13:48:24 +0600

    Add Docker

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import asyncio
-import platform
-from asyncio import WindowsSelectorEventLoopPolicy
-
 import uvicorn
 
 from app.app import get_asgi_application
 
 if __name__ == '__main__':
-    if platform.system() == 'Windows':
-        asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-
     uvicorn.run(get_asgi_application())
